9b3043c320ab1430cab58a682fb1a11830255375
Ahmed Hassan <54697653+ahmedhassan5291520@users.noreply.github.com>
2024-09-02 18:02:50 -0700

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,30 +1,28 @@
-    'use client'
-    import { headerItems, projects } from "@/constants/constant";
-    import Image from "next/image";
+'use client';
+import { headerItems, projects } from "@/constants/constant";
+import Image from "next/image";
 
-    const Projects: React.FC = () => {
+const Projects: React.FC = () => {
+  return (
+    <section
+      id={headerItems.projects.page}
+      className="flex-col text-center justify-center items-center my-40"
+    >
+      <h1 className="text-6xl my-6">Projects</h1>
+      <div className="flex break-words flex-wrap text-center justify-center items-center">
+        {Object.entries(projects).map(([projectName, projectData]) => (
+          <Image
+            key={projectName} // Unique key using project name
+            src={projectData.image}
+            alt=""
+            width={300}
+            height={300}
+            className="mt-4 md:m-1 cursor-pointer"
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
 
-        return (
-            <section 
-                id={headerItems.projects.page} 
-                className=" flex-col text-center justify-center items-center my-40"
-            >
-                <h1 className={"text-6xl my-6"}>Projects</h1>
-                <div className="flex break-words flex-wrap text-center justify-center items-center">
-                    {
-                        Object.keys(projects).map(project => (
-                            <Image 
-                                src={projects[project as keyof Projects].image} 
-                                alt=""  
-                                width={300}
-                                height={300}
-                                className="mt-4 md:m-1 cursor-pointer"
-                            />
-                        ))
-                    }
-                </div>
-            </section>
-        )
-    }
-
-    export default Projects;
+export default Projects;
